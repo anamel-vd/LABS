09fe314fd66a95682608c6a082d8a9f4701d0676
anamel-vd <anamel-vd@users.noreply.github.com>
2024-01-24 16:08:22 +0100

    LAB | SQL Subqueries 24.01.24
    LAB | SQL Subqueries

---
+++ WEEK3/LAB _SQL_Subqueries.sql
@@ -0,0 +1,31 @@
+USE sakila;
+
+#Write SQL queries to perform the following tasks using the Sakila database:
+#Determine the number of copies of the film "Hunchback Impossible" that exist in the inventory system.
+SELECT
+    (SELECT COUNT(*)
+     FROM inventory
+     WHERE film_id = (SELECT film_id
+                      FROM film
+                      WHERE title = 'Hunchback Impossible')) AS number_of_copies;
+
+#List all films whose length is longer than the average length of all the films in the Sakila database.
+
+SELECT
+    film_id, title, length
+FROM
+    film
+WHERE
+    length > (SELECT AVG(length) FROM film);
+
+#Use a subquery to display all actors who appear in the film "Alone Trip".
+
+SELECT
+    actor_id, first_name, last_name
+FROM actor
+WHERE actor_id IN (SELECT actor_id
+                   FROM film_actor
+                   WHERE film_id = (SELECT film_id
+                                    FROM film
+                                    WHERE title = 'Alone Trip'));
+
